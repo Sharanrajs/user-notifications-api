2baa81a50eb2956409c4be418060fa94860da8eb
Sharan Raj s <137904889+Sharanrajs@users.noreply.github.com>
2024-11-21 10:59:31 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Set global prefix for the API
+  app.setGlobalPrefix('api');
+
+  // Start the server
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`Application is running on: http://localhost:${PORT}/api`);
+}
+bootstrap();
 
