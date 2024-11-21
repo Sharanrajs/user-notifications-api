07519fccae36892510bd3824e974bce7a21b2884
Sharan Raj s <137904889+Sharanrajs@users.noreply.github.com>
2024-11-21 11:06:27 +0530

    Create user-preferences.module.ts

---
+++ src/user-preferences/user-preferences.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserPreferencesController } from './user-preferences.controller';
+import { UserPreferencesService } from './user-preferences.service';
+import { UserPreference, UserPreferenceSchema } from './schemas/user-preference.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserPreference.name, schema: UserPreferenceSchema },
+    ]),
+  ],
+  controllers: [UserPreferencesController],
+  providers: [UserPreferencesService],
+})
+export class UserPreferencesModule {}
